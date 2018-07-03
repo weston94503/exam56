cfa87e49d7e9d93cfefe2bcdd821d1c2b40cb3f2
weston94503 <40749459+weston94503@users.noreply.github.com>
2018-07-03 14:03:51 +0800

    執行測驗的 migrate 文件以建立資料表

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,8 +3,6 @@
     <title>{{ config('backpack.base.project_name') }} Error 403</title>
     @extends('layouts.app') 
     @section('content')
-    <h1>403 Forbidden.</h1>
-    <p>您沒有權限可以執行目前的動作喔！</p>
     @endsection  
 
     <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
@@ -56,9 +54,10 @@
             <?php
               $default_error_message = "Please return to <a href='".url('')."'>our homepage</a>.";
             ?>
+            <p>您沒有權限可以執行目前的動作喔！</p>
             {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
-         </small>
-       </div>
+          </small>
+        </div>
       </div>
     </div>
   </body>
