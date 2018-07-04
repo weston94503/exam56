8e0dedbb57ab9cdda9e348f7345458ab61d6a6e7
weston94503 <40749459+weston94503@users.noreply.github.com>
2018-07-04 14:44:55 +0800

    讀出所有測驗

---
+++ resources/views/exam/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app') 
+@section('content')
+    <h1>{{ __('隨機題庫系統') }}</h1>
+    <h1>測驗一覽</h1>
+    <ul class="list-group">
+        @foreach($exams as $exam)
+            <li class="list-group-item">
+                {{ $exam->created_at->format("Y年m月d日") }} -
+                <a href="exam/{{ $exam->id }}">
+                    {{ $exam->title }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,11 @@
 |
  */
 
-Route::get('/', function () {
-    $name = 'weston';
-    $say  = '嗨！';
-    return view('welcome', compact('name', 'say'));
-});
+Route::get('/', 'ExamController@index')->name('index');
+Route::get('/home', 'ExamController@index')->name('home.index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
 Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::post('/exam', 'ExamController@store')->name('exam.store');
