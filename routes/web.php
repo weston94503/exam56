54f430206492c4d43de8d02c1d195ad05fe56f15
weston94503 <40749459+weston94503@users.noreply.github.com>
2018-07-04 11:39:33 +0800

    將資料寫進資料庫

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
-Route::get('/exam', 'ExamController@index')->name('welcome');
+Route::get('/exam', 'ExamController@index')->name('exam.index');
+Route::post('/exam', 'ExamController@store')->name('exam.store');
